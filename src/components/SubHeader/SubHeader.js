6fede7047831ef0d25b7f4e9229d50e0e0de6b7a
Shrushti Polekar <shrushtipolekar@gmail.com>
2022-03-19 23:33:40 +0530

    style: changes styles for links

--- src/components/SubHeader/SubHeader.js
+++ src/components/SubHeader/SubHeader.js
@@ -16,40 +16,40 @@ const SubHeader = () => {
                 <div className='subheader-section' onMouseLeave={(e) => showMakeupSubheaderDropdown(!makeupSubheaderDropdown)}>
                     <h5 className='subheader-title' onMouseOver={(e) => showMakeupSubheaderDropdown(!makeupSubheaderDropdown)} >Makeup</h5>
                     <div className='subheader-dropdown-makeup' style={{ display: `${makeupSubheaderDropdown ? 'flex' : 'none'}` }} >
-                        <Link to="/productlistingpage"><p className='subheader-items' onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Lipstick"})}>Lipstick</p></Link>
-                        <Link to="/productlistingpage"><p className='subheader-items' onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Foundation"})}>Foundation</p></Link>
-                        <Link to="/productlistingpage"><p className='subheader-items' onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Eyeshadow"})}>Eyeshadow</p></Link>
-                        <Link to="/productlistingpage"><p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Mascara"})}>Mascara</p></Link>
+                        <Link className="links"  to="/productlistingpage"><p className='subheader-items' onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Lipstick"})}>Lipstick</p></Link>
+                        <Link className="links"  to="/productlistingpage"><p className='subheader-items' onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Foundation"})}>Foundation</p></Link>
+                        <Link  className="links" to="/productlistingpage"><p className='subheader-items' onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Eyeshadow"})}>Eyeshadow</p></Link>
+                        <Link  className="links" to="/productlistingpage"><p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Mascara"})}>Mascara</p></Link>
                     </div>
                 </div>
                 <div className='subheader-section' onMouseLeave={(e) => showBodyCareSubheaderDropdown(!bodyCareSubheaderDropdown)}>
                     <h5 className='subheader-title' onMouseOver={(e) => showBodyCareSubheaderDropdown(!bodyCareSubheaderDropdown)}>Body care</h5>
                     <div className='subheader-dropdown-skincare' style={{ display: `${bodyCareSubheaderDropdown ? 'flex' : 'none'}` }} >
-                         <Link to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"BodyLotions"})}>Body Lotions</p></Link>
-                         <Link to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"BodyWash"})}>Body wash</p></Link>
+                         <Link className="links"  to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"BodyLotions"})}>Body Lotions</p></Link>
+                         <Link className="links"  to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"BodyWash"})}>Body wash</p></Link>
                     </div>
                 </div>
                 <div className='subheader-section' onMouseLeave={(e) => showHairCareSubheaderDropdown(!hairCareSubheaderDropdown)}>
                     <h5 className='subheader-title' onMouseOver={(e) => showHairCareSubheaderDropdown(!hairCareSubheaderDropdown)} >Hair care</h5>
                     <div className='subheader-dropdown-haircare' style={{ display: `${hairCareSubheaderDropdown ? 'flex' : 'none'}` }} >
-                         <Link to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Shampoo"})}>Shampoos</p></Link>
-                         <Link to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"HairOil"})}>Hair oils</p></Link>
+                         <Link className="links" to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Shampoo"})}>Shampoos</p></Link>
+                         <Link className="links" to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"HairOil"})}>Hair oils</p></Link>
                     </div>
                 </div>
                 <div className='subheader-section' onMouseLeave={(e) => showSkinCareSubheaderDropdown(!skinCareSubheaderDropdown)}>
                     <h5 className='subheader-title' onMouseOver={(e) => showSkinCareSubheaderDropdown(!skinCareSubheaderDropdown)}>Skin care</h5>
                     <div className='subheader-dropdown-skincare' style={{ display: `${skinCareSubheaderDropdown ? 'flex' : 'none'}` }} >
-                         <Link to="/productlistingpage"> <p className='subheader-items' onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"FaceWash"})}>face Wash</p></Link>
-                         <Link to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Sunscreen"})}>Sun Screen</p></Link>
-                         <Link to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Moisturizer"})}>Moisturizers</p></Link>
-                         <Link to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"FaceCream"})}>Day Creams</p></Link>
+                         <Link className="links" to="/productlistingpage"> <p className='subheader-items' onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"FaceWash"})}>face Wash</p></Link>
+                         <Link className="links" to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Sunscreen"})}>Sun Screen</p></Link>
+                         <Link className="links" to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Moisturizer"})}>Moisturizers</p></Link>
+                         <Link className="links" to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"FaceCream"})}>Day Creams</p></Link>
                     </div>
                 </div>
                 <div className='subheader-section' onMouseLeave={(e) => showMenstrualHygieneSubheaderDropdown(!menstrualHygieneSubheaderDropdown)}>
                     <h5 className='subheader-title' onMouseOver={(e) => showMenstrualHygieneSubheaderDropdown(!menstrualHygieneSubheaderDropdown)}>Menstrual Hygiene</h5>
                     <div className='subheader-dropdown-menstrual-hygiene' style={{ display: `${menstrualHygieneSubheaderDropdown ? 'flex' : 'none'}` }}  >
-                         <Link to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"SanitaryPads"})}>Sanitary pads</p></Link>
-                         <Link to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Tampoons"})}>Tampoons</p></Link>
+                         <Link className="links" to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"SanitaryPads"})}>Sanitary pads</p></Link>
+                         <Link className="links" to="/productlistingpage"> <p className='subheader-items'  onClick={()=>dispatch({type:'SET_SUB_CATEGORY',payload:"Tampoons"})}>Tampoons</p></Link>
                     </div>
                 </div>
                 <div className='subheader-section'>
