beefea2a3b875c9ec503c5050fa95777a51cdd57
Shrushti Polekar <shrushtipolekar@gmail.com>
2022-03-21 19:19:09 +0530

    refactor: removed commented code

--- src/Reducer.js
+++ src/Reducer.js
@@ -3,21 +3,19 @@ import React from 'react'
 const stateReducer = (state, action) => {
     switch (action.type) {
         case 'SET_PRODUCTS': return { ...state, products: action.payload }
-        
-        case 'SET_CATEGORIES':return {...state,categories:action.payload}
-        
+
+        case 'SET_CATEGORIES': return { ...state, categories: action.payload }
+
         case 'SET_PRICE_RANGE': return { ...state, priceRange: action.payload }
-        
+
         case 'SET_SORT_TYPE': return { ...state, sortBy: action.payload }
-       
+
         case 'SET_CATEGORY': return state.sortByFilters.sortByCategory.includes(action.payload) ? { ...state, sortByFilters: { ...state.sortByFilters, sortByCategory: state.sortByFilters.sortByCategory.filter(item => item !== action.payload) } } :
             { ...state, sortByFilters: { ...state.sortByFilters, sortByCategory: state.sortByFilters.sortByCategory.concat(action.payload) } }
-           
-        // case 'SET_SUB_CATEGORY':return state.sortByFilters.sortBySubCategory.includes(action.payload) ? { ...state, sortByFilters: { ...state.sortByFilters, sortBySubCategory: state.sortByFilters.sortBySubCategory.filter(item => item !== action.payload) } } :
-        //         { ...state, sortByFilters: { ...state.sortByFilters, sortBySubCategory: state.sortByFilters.sortBySubCategory.concat(action.payload) } }
-        case 'SET_SUB_CATEGORY':return{...state,subCategory:action.payload}
+
+        case 'SET_SUB_CATEGORY': return { ...state, subCategory: action.payload }
         case 'SET_RATING': return { ...state, rating: action.payload }
-        case 'SET_BRAND':return  state.sortByFilters.sortByBrands.includes(action.payload) ? { ...state, sortByFilters: { ...state.sortByFilters, sortByBrands: state.sortByFilters.sortByBrands.filter(item => item !== action.payload) } } :
+        case 'SET_BRAND': return state.sortByFilters.sortByBrands.includes(action.payload) ? { ...state, sortByFilters: { ...state.sortByFilters, sortByBrands: state.sortByFilters.sortByBrands.filter(item => item !== action.payload) } } :
             { ...state, sortByFilters: { ...state.sortByFilters, sortByBrands: state.sortByFilters.sortByBrands.concat(action.payload) } }
         case 'CLEAR_ALL_FILTERS': return {
             ...state,
@@ -26,10 +24,10 @@ const stateReducer = (state, action) => {
             rating: 0,
             sortByFilters: {
                 sortByCategory: [],
-                sortByBrands:[],
-               
+                sortByBrands: [],
+
             },
-            subCategory:null
+            subCategory: null
         }
         default: return state
     }
