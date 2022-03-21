6ad23e2e8ab1467d1a7cd7276409b61f631040d6
Shrushti Polekar <shrushtipolekar@gmail.com>
2022-03-21 20:24:41 +0530

    feat:added basic signup auth without input field validation

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,30 +1,57 @@
+import axios from 'axios';
 import React from 'react'
+import { useState } from 'react';
 import './Signup.css';
+import { useNavigate } from 'react-router-dom';
+
 
 const Signup = () => {
+    const [firstName,setFirstName]=useState('')
+    const [lastName,setLastName]=useState('')
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    let navigate = useNavigate();
+    const signupHandler = async (e) => {
+       
+        try {
+          const response = await axios.post(`/api/auth/signup`, {
+            firstName,
+            lastName,
+            email,
+            password,
+          })
+          console.log(response)
+          if(response.status===201){
+            localStorage.setItem("token", response.data.encodedToken);
+            navigate('/signin')
+          }
+         } catch (error) {
+          console.log(error);
+        }
+      };
     return (
         <>
             <form class="form-container">
                 <h5 class="sub-heading">Signup</h5>
                 <div class="form-group flex-vt">
                     <label for="name-input" class="form-label form-field-required">Name</label>
-                    <input type="text" id="name-input" class="form-control" placeholder="enter name" required />
+                    <input type="text" id="name-input" class="form-control" placeholder="enter name" required onChange={(e)=>setFirstName(e.target.value)}/>
                 </div>
                 <div class="form-group flex-vt">
-                    <label for="name-input" class="form-label form-field-required">Surname</label>
-                    <input type="text" id="name-input" class="form-control" placeholder="enter name" required />
+                    <label for="name-input" class="form-label form-field-required" >Surname</label>
+                    <input type="text" id="name-input" class="form-control" placeholder="enter name" required onChange={(e)=>setLastName(e.target.value)}/>
                 </div>
                 <div class="form-group flex-vt">
-                    <label for="email-input" class="form-label form-field-required">Email</label>
-                    <input type="email" id="email-input" class="form-control" placeholder="abc@gmail.com" required />
+                    <label for="email-input" class="form-label form-field-required" >Email</label>
+                    <input type="email" id="email-input" class="form-control" placeholder="abc@gmail.com" required onChange={(e)=>setEmail(e.target.value)}/>
                     <small class="form-text text-mutated">We will never share your email with anyone.</small>
                 </div>
                 <div class="form-group flex-vt">
-                    <label for="password-input" class="form-label form-field-required">Password</label>
-                    <input type="password" id="password-input" class="form-control" placeholder="enter password" />
+                    <label for="password-input" class="form-label form-field-required" >Password</label>
+                    <input type="password" id="password-input" class="form-control" placeholder="enter password" onChange={(e)=>setPassword(e.target.value)} />
                 </div>
                 <label class="terms-condt-checkbox"> <input type="checkbox" />I accept all terms and conditions</label>
-                <button class="btn btn-primary">Submit</button>
+                <button class="btn btn-primary" onClick={signupHandler}>Submit</button>
                 <a href="../signin/signin.html" class="text-link">Already have an account?</a>
             </form>
         </>
