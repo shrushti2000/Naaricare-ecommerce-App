9021b89a2f297e5785179523fdaa173cc749666c
Shrushti Polekar <shrushtipolekar@gmail.com>
2022-03-21 19:46:26 +0530

    feat UI: added name and surname fields in signup

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -2,25 +2,33 @@ import React from 'react'
 import './Signup.css';
 
 const Signup = () => {
-  return (
-    <>
-     <form class="form-container">
-        <h5 class="sub-heading">Signup</h5>
-        <div class="form-group flex-vt">
-            <label for="email-input" class="form-label form-field-required">Email</label>
-            <input type="email" id="email-input" class="form-control" placeholder="abc@gmail.com" required />
-            <small class="form-text text-mutated">We will never share your email with anyone.</small>
-        </div>
-        <div class="form-group flex-vt">
-            <label for="password-input" class="form-label form-field-required">Password</label>
-            <input type="password" id="password-input" class="form-control" placeholder="enter password" />
-        </div>
-        <label class="terms-condt-checkbox"> <input type="checkbox" />I accept all terms and conditions</label>
-        <button class="btn btn-primary">Submit</button>
-        <a href="../signin/signin.html" class="text-link">Already have an account?</a>
-    </form>
-    </>
-  )
+    return (
+        <>
+            <form class="form-container">
+                <h5 class="sub-heading">Signup</h5>
+                <div class="form-group flex-vt">
+                    <label for="name-input" class="form-label form-field-required">Name</label>
+                    <input type="text" id="name-input" class="form-control" placeholder="enter name" required />
+                </div>
+                <div class="form-group flex-vt">
+                    <label for="name-input" class="form-label form-field-required">Surname</label>
+                    <input type="text" id="name-input" class="form-control" placeholder="enter name" required />
+                </div>
+                <div class="form-group flex-vt">
+                    <label for="email-input" class="form-label form-field-required">Email</label>
+                    <input type="email" id="email-input" class="form-control" placeholder="abc@gmail.com" required />
+                    <small class="form-text text-mutated">We will never share your email with anyone.</small>
+                </div>
+                <div class="form-group flex-vt">
+                    <label for="password-input" class="form-label form-field-required">Password</label>
+                    <input type="password" id="password-input" class="form-control" placeholder="enter password" />
+                </div>
+                <label class="terms-condt-checkbox"> <input type="checkbox" />I accept all terms and conditions</label>
+                <button class="btn btn-primary">Submit</button>
+                <a href="../signin/signin.html" class="text-link">Already have an account?</a>
+            </form>
+        </>
+    )
 }
 
 export default Signup
