f6a73fdbcc45a0fbc9461680266c1856b0200125
Shrushti Polekar <shrushtipolekar@gmail.com>
2022-03-21 19:21:08 +0530

    refactor: refactored imports

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import SubHeader from '../../components/SubHeader/SubHeader'
 import Carousel from '../../components/Carousel/Carousel'
-
 import brandImage1 from '../../assets/Images/loreal_logo.jpg'
 import brandImage2 from '../../assets/Images/lakme_logo.jpg'
 import brandImage4 from '../../assets/Images/maybelline_logo.png'
@@ -13,9 +12,8 @@ import offerImage2 from '../../assets/Images/offerImage2.jpg'
 import { Link } from 'react-router-dom'
 import './HomePage.css'
 import Footer from '../../components/Footer/Footer'
-import { useContext } from 'react'
+import { useContext,useEffect } from 'react'
 import { StateContext } from '../../Context'
-import { useEffect } from 'react'
 import Categories from '../../components/Categories/Categories'
 
 const HomePage = () => {
