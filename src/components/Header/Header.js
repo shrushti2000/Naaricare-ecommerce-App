db86b452d99046c61540a52d38415b586c7ae6e9
Shrushti Polekar <shrushtipolekar@gmail.com>
2022-03-28 11:55:59 +0530

    feat:added logout feature

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { StateContext } from '../../Context'
 import Searchbar from '../Searchbar/Searchbar'
 import './Header.css'
-import AuthProvider from '../../AuthProvider'
+import { AuthContext } from '../../AuthProvider'
+
 
 const Header = () => {
   const {state}=useContext(StateContext)
+  const {token,setToken}=useContext(AuthContext)
+ 
+  let navigate = useNavigate();
+  const handleLogout=()=>{
+   localStorage.clear()
+    setToken(undefined)
+    navigate('/')
+  }
 
   return (
     <>
@@ -17,13 +26,21 @@ const Header = () => {
         </div>
         <i class="fa fa-bars"></i>
         <Searchbar />
-        <div class="navigation-item-3 flex-hz">
-          <Link to="/signin"> <button class="btn btn-primary">Login</button></Link>
+       {token===undefined ? <>
+       <div class="navigation-item-3 flex-hz">
+          <Link to="/signin"> <button class="btn btn-primary">Signin</button></Link>
           <Link to="/signup"> <button class="btn btn-primary">Signup</button></Link>
-          <Link to="/cartpage"><i className="fa fa-shopping-cart"><span class="badge-w-icon">{state.cart.length}</span></i></Link>
+       
+          <i class="fa fa-search search-icon-responsive"></i>
+        </div>
+       </>:<>
+        <div class="navigation-item-3 flex-hz">
+           <button class="btn btn-primary" onClick={handleLogout}>Logout</button>
+           <Link to="/cartpage"><i className="fa fa-shopping-cart"><span class="badge-w-icon">{state.cart.length}</span></i></Link>
           <Link to="/wishlistpage" ><i class="fa fa-heart"><span class="badge-w-icon">{state.wishlist.length}</span></i></Link>
           <i class="fa fa-search search-icon-responsive"></i>
         </div>
+       </>}
       </div>
     </>
   )
